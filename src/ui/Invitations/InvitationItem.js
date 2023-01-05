93f8cf8a3e4cae5b65447fe8825fc7302dce335c
Amit Kumar <ellipticamit@gmail.com>
2023-01-05 21:00:34 +0530

    button updated invitation

--- src/ui/Invitations/InvitationItem.js
+++ src/ui/Invitations/InvitationItem.js
@@ -9,7 +9,7 @@ const InvitationItem = () => {
         become his family member.
       </div>
       <div className='flex items-center space-x-2 text-white font-semibold text-2xl'>
-        <div className='bg-success-dark w-8 h-8 md:w-9 md:h-9 rounded-md flex items-center justify-center cursor-pointer'>
+        <div className='bg-success-dark w-8 h-8 rounded-md flex items-center justify-center cursor-pointer'>
           <div className=''>
             <img src='/correct.svg' alt='' />
           </div>
